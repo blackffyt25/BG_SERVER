731bf2655d0e7b85c62a13948ae2c06873029ef1
blackffyt25 <blackffyt25@gmail.com>
2026-05-13 13:22:23 +0530

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -23,5 +26,5 @@ app.post("/build", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log("Server running on port " + PORT);
 });
